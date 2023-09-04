b276254321268c985b94a61301abf7066220985e
rolins2 <127750238+rolins2@users.noreply.github.com>
2023-09-04 18:21:48 +0530

    Chore: single components

--- ny-expensetracker/src/components/ExpenseItem.js
+++ ny-expensetracker/src/components/ExpenseItem.js
@@ -1,11 +1,18 @@
 import "./ExpenseItem.css";
-function ExpenseItem() {
+function ExpenseItem(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
   return (
-    <div className="expenseitem">
-      <div>Date</div>
-      <div className="expenseitem__description">
-        <h2> Title </h2>
-        <div className="expenseitem__price"> Anount </div>
+    <div className="expense-item">
+      <div>
+        <div>{month} </div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
+      <div className="expense-item__description">
+        <h2> {props.title} </h2>
+        <div className="expense-item__price "> {props.amount} </div>
       </div>
     </div>
   );
