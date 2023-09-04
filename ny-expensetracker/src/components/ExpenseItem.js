b44a81c55380187655bbba2c5a88f273d0abd9dc
rolins2 <127750238+rolins2@users.noreply.github.com>
2023-09-04 18:51:49 +0530

    Style: added some styling to expenses date .css

--- ny-expensetracker/src/components/ExpenseItem.js
+++ ny-expensetracker/src/components/ExpenseItem.js
@@ -1,18 +1,14 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = props.date.getFullYear();
   return (
     <div className="expense-item">
       <div>
-        <div>{month} </div>
-        <div>{year}</div>
-        <div>{day}</div>
+        <ExpenseDate date={props.date}> </ExpenseDate>
       </div>
       <div className="expense-item__description">
         <h2> {props.title} </h2>
-        <div className="expense-item__price "> {props.amount} </div>
+        <div className="expense-item__price ">Rs: {props.amount} </div>
       </div>
     </div>
   );
