cd92192c08eb5ba582ea1716f7666d670077f288
rolins2 <127750238+rolins2@users.noreply.github.com>
2023-09-06 18:53:24 +0530

    Feat : Added the card componet

--- ny-expensetracker/src/App.js
+++ ny-expensetracker/src/App.js
@@ -1,6 +1,5 @@
-import ExpenseItem from "./components/ExpenseItem";
 import "./App.css";
-
+import Expenses from "./components/Expenses";
 function App() {
   const expenses = [
     {
@@ -26,32 +25,8 @@ function App() {
   return (
     <div className="App">
       <h2> We start here</h2>
-      <div>
-        <ExpenseItem
-          title={expenses[0].title}
-          amount={expenses[0].amount}
-          date={expenses[0].date}
-        >
-          {" "}
-        </ExpenseItem>
-        <ExpenseItem
-          title={expenses[1].title}
-          amount={expenses[1].amount}
-          date={expenses[1].date}
-        ></ExpenseItem>
 
-        <ExpenseItem
-          title={expenses[2].title}
-          amount={expenses[2].amount}
-          date={expenses[2].date}
-        ></ExpenseItem>
-
-        <ExpenseItem
-          title={expenses[3].title}
-          amount={expenses[3].amount}
-          date={expenses[3].date}
-        ></ExpenseItem>
-      </div>
+      <Expenses expenses={expenses}></Expenses>
     </div>
   );
 }
--- ny-expensetracker/src/components/ExpenseItem.js
+++ ny-expensetracker/src/components/ExpenseItem.js
@@ -1,8 +1,9 @@
 import "./ExpenseItem.css";
+import Card from "./Card";
 import ExpenseDate from "./ExpenseDate";
 function ExpenseItem(props) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <div>
         <ExpenseDate date={props.date}> </ExpenseDate>
       </div>
@@ -10,7 +11,7 @@ function ExpenseItem(props) {
         <h2> {props.title} </h2>
         <div className="expense-item__price ">Rs: {props.amount} </div>
       </div>
-    </div>
+    </Card>
   );
 }
 
