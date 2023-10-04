dac2bd4825d35efb7b29d748c9f0123f4b6601f8
rolins2 <127750238+rolins2@users.noreply.github.com>
2023-10-04 16:50:30 +0530

    Added Use state
    Learnt how useState works

--- ny-expensetracker/src/components/ExpenseItem.js
+++ ny-expensetracker/src/components/ExpenseItem.js
@@ -1,17 +1,20 @@
+import { useState } from "react";
 import "./ExpenseItem.css";
 import Card from "./Card";
 import ExpenseDate from "./ExpenseDate";
 function ExpenseItem(props) {
   function clickHandler() {
     console.log("Checking if click handler works");
+    setTtl("Chungus");
   }
+  const [ttl, setTtl] = useState(props.title);
   return (
     <Card className="expense-item">
       <div>
         <ExpenseDate date={props.date}> </ExpenseDate>
       </div>
       <div className="expense-item__description">
-        <h2> {props.title} </h2>
+        <h2> {ttl} </h2>
         <div className="expense-item__price ">Rs: {props.amount} </div>
       </div>
       <button onClick={clickHandler}> Hello Button </button>
