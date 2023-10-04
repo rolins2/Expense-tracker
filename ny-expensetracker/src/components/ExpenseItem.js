962e5828d810a31bacda00b294a46f9bb63aa229
rolins2 <127750238+rolins2@users.noreply.github.com>
2023-10-04 16:33:12 +0530

    Added a temp button
    Added a temporary button to learn click Handler

--- ny-expensetracker/src/components/ExpenseItem.js
+++ ny-expensetracker/src/components/ExpenseItem.js
@@ -2,6 +2,9 @@ import "./ExpenseItem.css";
 import Card from "./Card";
 import ExpenseDate from "./ExpenseDate";
 function ExpenseItem(props) {
+  function clickHandler() {
+    console.log("Checking if click handler works");
+  }
   return (
     <Card className="expense-item">
       <div>
@@ -11,6 +14,7 @@ function ExpenseItem(props) {
         <h2> {props.title} </h2>
         <div className="expense-item__price ">Rs: {props.amount} </div>
       </div>
+      <button onClick={clickHandler}> Hello Button </button>
     </Card>
   );
 }
