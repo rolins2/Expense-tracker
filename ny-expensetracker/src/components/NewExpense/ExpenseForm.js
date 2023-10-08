f9116f67711eb9b6987dc595486cc9f12971b787
rolins2 <127750238+rolins2@users.noreply.github.com>
2023-10-09 01:08:12 +0530

    Feat: Added use state

--- ny-expensetracker/src/components/NewExpense/ExpenseForm.js
+++ ny-expensetracker/src/components/NewExpense/ExpenseForm.js
@@ -1,22 +1,46 @@
+import { useState } from "react";
 function ExpenseForm() {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+  function titleChangeHandler(event) {
+    setEnteredTitle(event.target.value);
+  }
+  function amountChangeHandler(event) {
+    setEnteredAmount(event.target.value);
+  }
+  function dateChangeHandler(event) {
+    setEnteredDate(event.target.value);
+  }
+  function submitHandler(event) {
+    event.preventDefault();
+    console.log(
+      "The entered title is " +
+        enteredTitle +
+        "\nThe entered amount is " +
+        enteredAmount +
+        "\nThe entered date is " +
+        enteredDate
+    );
+  }
   return (
     <div>
-      <form>
+      <form onSubmit={submitHandler}>
         <div>
           <div>
             <label>Title </label>
-            <input type="text"></input>
+            <input type="text" onChange={titleChangeHandler}></input>
           </div>
           <div>
             <label>Amount</label>
-            <input type="text" />
+            <input type="text" onChange={amountChangeHandler} />
           </div>
           <div>
             <label>Date</label>
-            <input type="date" />
+            <input type="date" onChange={dateChangeHandler} />
           </div>
           <div>
-            <button>Submit</button>
+            <button type="submit">Submit</button>
           </div>
         </div>
       </form>
