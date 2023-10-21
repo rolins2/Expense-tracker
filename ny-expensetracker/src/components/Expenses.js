84821e39800758573d6d967b71c38427c1143bb9
rolins2 <127750238+rolins2@users.noreply.github.com>
2023-10-22 03:37:35 +0530

    feat: added key to the map function

--- ny-expensetracker/src/components/Expenses.js
+++ ny-expensetracker/src/components/Expenses.js
@@ -19,6 +19,7 @@ function Expenses(props) {
           title={expense.title}
           amount={expense.amount}
           date={expense.date}
+          key={expense.id}
         />
       ))}
       /
